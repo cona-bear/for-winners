540075f104c951749bd611add28aef366dc6777c
cona-bear <seonghyeon.koo@bearrobotics.ai>
2025-03-07 14:58:31 +0900

    add wordle & kordle button in message

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
 import os
 import random
 import time
+import pytz 
+import datetime
 
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
@@ -13,7 +15,7 @@ from webdriver_manager.chrome import ChromeDriverManager
 SLACK_TOKEN = os.getenv("SLACK_TOKEN")
 CHANNEL_ID = os.getenv("CHANNEL_ID")
 
-# Set up Selenium WebDriver
+
 def get_random_problem():
     options = webdriver.ChromeOptions()
     options.add_argument("--headless")
@@ -49,20 +51,76 @@ def get_random_problem():
             return "No problems found."
 
         problem = random.choice(problems)
-        return f"*Today's Problem:* {problem['title']} - {problem['url']}"
+        return problem
 
     except Exception as e:
         driver.quit()
         return f"Error: {str(e)}"
 
 
-problem = get_random_problem()
+def build_message(problem):
 
-client = WebClient(token=SLACK_TOKEN)
-try:
-    response = client.chat_postMessage(
-        channel=CHANNEL_ID,
-        text=problem
-    )
-except SlackApiError as e:
-    assert e.response["error"]
+    kst = pytz.timezone("Asia/Seoul")
+    today = datetime.datetime.now(kst).strftime("%m/%d")
+
+    return [
+        {
+			"type": "header",
+			"text": {
+				"type": "plain_text",
+				"text": f":maru_yum: {today} Today's Problem :maru_yum:"
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": f"{problem['title']} - {problem['url']}"
+			}
+		},
+		{
+			"type": "actions",
+			"elements": [
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": ":capital_abcd: WORDLE"
+					},
+					"url": "https://www.nytimes.com/games/wordle"
+				},
+				{
+					"type": "button",
+					"text": {
+						"type": "plain_text",
+						"text": ":jigsaw: 꼬들"
+					},
+					"url": "https://kordle.kr"
+				}
+			]
+		},
+		{
+			"type": "divider"
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "cona luvs you. maru is a puppy. :maru_heart:"
+			}
+		},
+    ]
+
+
+if __name__ == "__main__":
+    client = WebClient(token=SLACK_TOKEN)
+    
+    problem = get_random_problem()
+    try:
+        response = client.chat_postMessage(
+            channel=CHANNEL_ID,
+            blocks=build_message(problem),
+            unfurl_links=False, # Don't show preview of the link
+        )
+    except SlackApiError as e:
+        assert e.response["error"]
